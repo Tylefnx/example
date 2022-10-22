301bd0b1e153eabed66a5bd6d90a247ee6557777
Tylefnx <tayfunucuncu@gmail.com>
2022-10-22 18:27:36 +0300

    Making things easier to understand

--- lib/weather/infrastructure/weather_repository.dart
+++ lib/weather/infrastructure/weather_repository.dart
@@ -8,16 +8,20 @@ import 'package:example/weather/infrastructure/weather_service.dart';
 class WeatherRepository {
   final WeatherService service = WeatherService();
   Future<Either<WeatherFailure, Weather>> fetchCategories() async {
-
     int randomResponse = Random().nextInt(6);
 
     switch (randomResponse) {
       case 1:
-        return left(const WeatherFailure.server(
-            error: 'There is a problem with the server'));
+        return left(
+          const WeatherFailure.server(
+              error: 'There is a problem with the server'),
+        );
+        
       case 2:
-        return left(const WeatherFailure.storage(error: "No internet"));
-      
+        return left(
+          const WeatherFailure.storage(error: "No internet"),
+        );
+
       default:
         return right(service.fetchWeather());
     }
