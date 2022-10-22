b73c7576159053577e730a0a91caa64b6ac6b9ef
Tylefnx <tayfunucuncu@gmail.com>
2022-10-22 19:29:16 +0300

    Forgot to add ProviderScope to the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:example/core/presentation/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(const AppWidget());
+void main() => runApp(
+      const ProviderScope(
+        child: AppWidget(),
+      ),
+    );
